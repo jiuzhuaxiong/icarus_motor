b13d7c4072dbc19d20ceb06d6e077e45e74fc691
Andrew Li <al2114@ic.ac.uk>
2017-03-21 18:20:20 +0000

    Added quadrature state

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@
 #define I3pin D12
 
 //Incremental encoder input pins
-#define CHA   D7
-#define CHB   D8  
+#define CHApin D7
+#define CHBpin D8  
 
 //Motor Drive output pins   //Mask in output byte
-#define L1Lpin D4           //0x01
-#define L1Hpin D5           //0x02
+#define L1Lpin D4           //0x01   PROBLEM? (Left, 2nd from bottom)
+#define L1Hpin D5           //0x02   PROBLEM? Or good
 #define L2Lpin D3           //0x04
-#define L2Hpin D6           //0x08
+#define L2Hpin D6           //0x08   PROBLEM? (Left, 4th from bottom)
 #define L3Lpin D9           //0x10
 #define L3Hpin D10          //0x20
 
@@ -34,7 +34,7 @@ State   L1  L2  L3
 const int8_t driveTable[] = {0x12,0x18,0x09,0x21,0x24,0x06,0x00,0x00};
 
 //Mapping from interrupter inputs to sequential rotor states. 0x00 and 0x07 are not valid
-const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};  
+const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x08};  
 //const int8_t stateMap[] = {0x07,0x01,0x03,0x02,0x05,0x00,0x04,0x07}; //Alternative if phase order of input or drive is reversed
 
 //Phase lead to make motor spin
@@ -48,6 +48,10 @@ DigitalIn I1(I1pin);
 DigitalIn I2(I2pin);
 DigitalIn I3(I3pin);
 
+//Quadrature inputs
+InterruptIn CHA(CHApin);
+InterruptIn CHB(CHBpin);
+
 //Motor Drive outputs
 DigitalOut L1L(L1Lpin);
 DigitalOut L1H(L1Hpin);
@@ -56,12 +60,17 @@ DigitalOut L2H(L2Hpin);
 DigitalOut L3L(L3Lpin);
 DigitalOut L3H(L3Hpin);
 
+//Serial output
+Serial pc(SERIAL_TX, SERIAL_RX);
+
 //Set a given drive state
 void motorOut(int8_t driveState){
     
     //Lookup the output byte from the drive state.
     int8_t driveOut = driveTable[driveState & 0x07];
-      
+
+    pc.printf("motorOut: %x\n\r",driveState);
+
     //Turn off first
     if (~driveOut & 0x01) L1L = 0;
     if (~driveOut & 0x02) L1H = 1;
@@ -93,29 +102,73 @@ int8_t motorHome() {
     //Get the rotor state
     return readRotorState();
 }
-    
+   
+int tick = 0;
+
+Timer t;
+
+
+int inc[2] = {-1,1};
+
+inline void CHA_rise_isr() {
+    tick -= inc[CHB.read()];
+}
+
+inline void CHA_fall_isr() {
+    tick += inc[CHB.read()];
+}
+
+inline void CHB_rise_isr() {
+    tick += inc[CHA.read()];
+}
+
+inline void CHB_fall_isr() {
+    tick -= inc[CHA.read()];
+
+    // Maybe faster?
+    // int val = CHA.read();
+    // tick += (1>>val);
+    // tick -= (1>>!val);
+}
+
 //Main
 int main() {
     int8_t orState = 0;    //Rotot offset at motor state 0
     
     //Initialise the serial port
-    Serial pc(SERIAL_TX, SERIAL_RX);
     int8_t intState = 0;
     int8_t intStateOld = 0;
+    int8_t motorspin = 0;
     pc.printf("Hello\n\r");
     
     //Run the motor synchronisation
     orState = motorHome();
     pc.printf("Rotor origin: %x\n\r",orState);
     //orState is subtracted from future rotor state inputs to align rotor and motor states
-    
+
+    CHA.rise(&CHA_rise_isr);
+    CHA.fall(&CHA_fall_isr);
+    CHB.rise(&CHB_rise_isr);
+    CHB.fall(&CHB_fall_isr);
+
     //Poll the rotor state and set the motor outputs accordingly to spin the motor
     while (1) {
-        intState = readRotorState();
-        if (intState != intStateOld) {
-            intStateOld = intState;
-            motorOut((intState-orState+lead+6)%6); //+6 to make sure the remainder is positive
-        }
+
+        pc.printf("%d\n\r",(tick%468+468)%468);
+        wait(0.1);
+        // pc.printf("%d\n\r",(tick%117));
+        // pc.printf("[%d,%d,%d]\n\r",(int)I3,(int)I2,(int)I1);
+        // wait(1.0);
+
+        // intState = readRotorState();
+        // if (intState != intStateOld) {
+        //     intStateOld = intState;
+        //     motorspin = (intState-orState+lead+6)%6;
+        //     pc.printf("Spin to: %x\n\r",motorspin);
+        //     // pc.printf("[%x,"CHA);
+        //     // pc.printf("%x]\n\r",CHB);
+        //     motorOut(motorspin); //+6 to make sure the remainder is positive
+        // }
     }
 }
 
